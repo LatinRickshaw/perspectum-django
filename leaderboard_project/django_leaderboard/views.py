13bda69e56e785796de62da5829ad0a2db352898
Christian <christian.fitzgibbon@gmail.com>
2025-03-25 13:35:36 +0000

    Feat: Added optional parameters for min and max submissions as env vars

--- leaderboard_project/django_leaderboard/views.py
+++ leaderboard_project/django_leaderboard/views.py
@@ -1,10 +1,11 @@
+import os
 from django.shortcuts import render
 from .services import LeaderboardService
 
-class LeaderboardView:
-    @staticmethod
-    def leaderboard(request):
-        ranked_users = LeaderboardService.get_leaderboard()
-        return render(request, 'leaderboard.html', {'leaderboard': ranked_users})
+def leaderboard_view(request):
+    min_submissions = int(os.getenv('MIN_SUBMISSIONS', 3))
+    max_submissions = int(os.getenv('MAX_SUBMISSIONS', 24))
     
-
+    leaderboard = LeaderboardService.get_leaderboard(min_submissions=min_submissions, max_submissions=max_submissions)
+    
+    return render(request, 'leaderboard.html', {'leaderboard': leaderboard})
